5b1405af76ed9af2a873ce94ddeaf5624550b6ec
sibusisiwe0405 <65665525+sibusisiwe0405@users.noreply.github.com>
2023-06-26 17:36:27 +0200

    save
    save

--- src/components/Netart.jsx
+++ src/components/Netart.jsx
@@ -6,7 +6,7 @@ import butterfly from "./Images/butterfly.jpeg"
   return (
     
         <div>
-          <h1>Artwork</h1>
+          <h1>Color Me</h1>
 
           <img src={butterfly} className="stylePic" alt="New Moodboard" style={{ width: '50%' }} />
         </div>
